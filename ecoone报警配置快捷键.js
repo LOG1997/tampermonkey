d2e6a65709d28162a35b922f147de858c5c19b4e
LOG1997 <2694233102@qq.com>
2022-06-08 12:29:43 +0800

    ecoonebaojing

--- ecoone报警配置快捷键.js
+++ ecoone报警配置快捷键.js
@@ -1,18 +1,19 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         ecoone报警配置快捷键
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  try to take over the world!
+// @version      0.2
+// @description  ecoone报警配置
 // @author       You
 // @match        https://www.chiiot.cn/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chiiot.cn
 // @grant        none
+// @license MIT
 // ==/UserScript==
 
 (function () {
   /*
    * @Author: LOG
- * @FilePath: \油猴脚本\ecoone报警配置快捷键.js
+   * @FilePath: \油猴脚本\ecoone报警配置快捷键.js
    * @Descripttion:
    * @version:
    * @Date: 2022-05-30 10:06:57
@@ -61,7 +62,7 @@
   function replaceText(text_old, text_new) {
     let braekInputCon =
       document.querySelectorAll(".el-input--small")[13].children[0];
-
+    console.log("replaceTSTSTSTST",text_old,text_new)
     let input_text = braekInputCon.value;
     let input_text_new = input_text.replace(text_old, text_new);
 
@@ -72,15 +73,21 @@
 
   // console.log("描述：", braekInputCon);
 
-  // 监听ctrl+1
-
+  // 旧文本和新文本
   let text_old = "";
   let text_new = "";
+  // 监听按键
   document.addEventListener("keydown", function (e) {
-    if ((e.keyCode == 49 || e.keyCode == 50 || e.keyCode == 51) && e.ctrlKey) {
+    if (
+      (e.keyCode == 49 ||
+        e.keyCode == 50 ||
+        e.keyCode == 51 ||
+        e.keyCode == 52) &&
+      e.ctrlKey
+    ) {
       e.preventDefault();
-      console.log("ctrl+1", e.keyCode);
-      // 全选
+      console.log("ctrl+1、2、3、4：", e.keyCode);
+      // 根据输入ctrl+数字的不同来选择不同等级的报警
       setTimeout(() => {
         clickBreak0(2);
       }, 200);
@@ -90,27 +97,23 @@
       setTimeout(() => {
         BreakInput();
         replaceText(text_old, text_new);
-      }, 200);
-      setTimeout(() => {
-        replaceText(text_old, text_new);
-      }, 500);
+      }, 300);
+      // 需要替换文本时才执行这部操作
+          console.log("新旧：",text_old,text_new)
+      if (text_old != ''&&text_new!='') {
+        setTimeout(() => {
+          replaceText(text_old, text_new);
+        }, 500);
+      }
       setTimeout(() => {
         clcikEnter();
       }, 1000);
     }
-    // ctrl+a
-    if (e.keyCode == 88 && e.ctrlKey) {
-      e.preventDefault();
-      console.log("ctrl+x", e.keyCode);
-      replaceText(text_old, text_new);
-      setTimeout(() => {
-        clcikEnter();
-      }, 500);
-    }
-    // ctrl+`
+    // ctrl+`输入需要旧的文本和新文本
     if (e.keyCode == 192 && e.ctrlKey) {
       text_old = window.prompt("原始文本: ");
       text_new = window.prompt("新文本: ");
     }
+    
   });
 })();
