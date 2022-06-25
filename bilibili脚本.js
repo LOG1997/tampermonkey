2cd1fe531536de33cff07fa9467642a7c2f66c58
LOG1997 <2694233102@qq.com>
2022-06-25 13:53:22 +0800

    add console

--- bilibili脚本.js
+++ bilibili脚本.js
@@ -1,8 +1,8 @@
 /*
  * @Author: LOG
  * @FilePath: \油猴脚本\bilibili脚本.js
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Date: 2022-06-08 20:44:17
  * @LastEditors: LOG
  * @LastEditTime: 2022-06-08 21:12:21
@@ -18,41 +18,42 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    // Your code here...
-    // bilibili网站加载成功后执行，去除一些模块
-    window.onload = function () {
-        //方法内容
-        let section_index=[6,7,8,9,10,11,13,15,16,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
-        let documentary=document.querySelectorAll(".bili-grid");
-        // console.log('纪录片',documentary);
-        for(let i=0;i<section_index.length;i++){
-            documentary[section_index[i]].remove();
-            // documentary[section_index[i]].style.display="none";
-        }
-        let bili_footer=document.querySelectorAll(".bili-footer");
-        // console.log('bili_footer',bili_footer);
-        bili_footer[0].remove();
-        // 执行刷新操作
-        RefreshRecommend();
+  // Your code here...
+  // bilibili网站加载成功后执行，去除一些模块
+  window.onload = function () {
+    //方法内容
+    let section_index = [
+      6, 7, 8, 9, 10, 11, 13, 15, 16, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+      30, 31, 32, 33,
+    ];
+    let documentary = document.querySelectorAll(".bili-grid");
+    // console.log('纪录片',documentary);
+    for (let i = 0; i < section_index.length; i++) {
+      documentary[section_index[i]].remove();
+      // documentary[section_index[i]].style.display="none";
     }
+    let bili_footer = document.querySelectorAll(".bili-footer");
+    // console.log('bili_footer',bili_footer);
+    bili_footer[0].remove();
+    console.log('%csuccess%c:','background:#47c00e;padding:3px;border-radius:3px','','脚本加载成功');
+    // 执行刷新操作
+    RefreshRecommend();
+  };
 
-    // 快捷键，快捷刷新推荐视频
-    function RefreshRecommend(){
-        // alt+r按键监听
-        document.addEventListener('keydown', function(event) {
-            if (event.altKey && event.keyCode == 82) {
-                event.preventDefault();
-                // console.log('alt+r');
-                // 快捷键按下后，执行的方法
-
-                let refresh_button=document.querySelectorAll(".roll-btn-wrap")[0];
-                console.log('refresh_button🚁🚁🚁', refresh_button);
-                refresh_button.click();
-            }
-        });
-    }
-
+  // 快捷键，快捷刷新推荐视频
+  function RefreshRecommend() {
+    // alt+r按键监听
+    document.addEventListener("keydown", function (event) {
+      if (event.altKey && event.keyCode == 82) {
+        event.preventDefault();
+        // console.log('alt+r');
+        // 快捷键按下后，执行的方法
+        let refresh_button = document.querySelectorAll(".roll-btn-wrap")[0].children[0];
+        refresh_button.click();
+      }
+    });
+  }
 })();
