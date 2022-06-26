d7aa5091a5d480c779520985b2b2b699f408fea8
LOG1997 <2694233102@qq.com>
2022-06-26 18:57:12 +0800

    feat(bili): 搜索页面的数字键点击卡片

--- bilibili脚本.js
+++ bilibili脚本.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         bilibili script
 // @namespace    http://tampermonkey.net/
-// @version      0.34
+// @version      0.35
 // @description  bilibili网站操作脚本
 // @author       You
 // @match        https://www.bilibili.com/
+// @match        https://search.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // @license MIT
@@ -16,7 +17,26 @@
   // Your code here...
   // bilibili网站加载成功后执行，去除一些模块
   window.onload = function () {
+    console.log(
+      "%csuccess%c:",
+      "background:#47c00e;padding:3px;border-radius:3px",
+      "",
+      "脚本加载成功"
+    );
     //方法内容
+    let currentUrl = window.location.href;
+    if (currentUrl.indexOf("https://www.bilibili.com/") == 0) {
+      // 移除首页多余卡片
+      RemoveCard();
+      // 执行快捷键刷新和数字键跳转视频操作
+      RefreshRecommend();
+    }
+    if (currentUrl.indexOf("https://search.bilibili.com/") == 0) {
+      // 执行快捷键刷新和数字键跳转视频操作
+      searchPageSelectCard();
+    }
+  };
+  function RemoveCard() {
     let section_index = [
       6, 7, 8, 9, 10, 11, 13, 15, 16, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
       30, 31, 32, 33,
@@ -30,22 +50,15 @@
     let bili_footer = document.querySelectorAll(".bili-footer");
     // console.log('bili_footer',bili_footer);
     bili_footer[0].remove();
-    console.log(
-      "%csuccess%c:",
-      "background:#47c00e;padding:3px;border-radius:3px",
-      "",
-      "脚本加载成功"
-    );
-    // 执行刷新操作
-    RefreshRecommend();
-  };
-
+  }
   // 快捷键，快捷刷新推荐视频
   function RefreshRecommend() {
     // alt+r按键监听
     document.addEventListener("keydown", function (event) {
       // 检测到光标聚焦于输入框时，则不触发快捷键事件。
-      if(['input', 'textarea'].indexOf(event.target.tagName.toLowerCase()) > -1){
+      if (
+        ["input", "textarea"].indexOf(event.target.tagName.toLowerCase()) > -1
+      ) {
         return false;
       }
       if (event.altKey && event.keyCode == 82) {
@@ -57,15 +70,75 @@
         refresh_button.click();
       }
       //   监听数字键盘
-      if (event.keyCode >= 48 && event.keyCode <= 57&&!event.ctrlKey) {
+      if (event.keyCode >= 48 && event.keyCode <= 57 && !event.ctrlKey) {
         event.preventDefault();
         let num = event.keyCode - 48 > 0 ? event.keyCode - 48 : 10;
         // 获取推荐视频的卡片
-        let recommend_video = document.querySelectorAll(
-          ".recommend-container__2-line"
-        )[0].children[num].children[1].children[0];
-        // 点击相应卡片
-        recommend_video.click();
+        try {
+          let recommend_video = document.querySelectorAll(
+            ".recommend-container__2-line"
+          )[0].children[num].children[1].children[0];
+          if (!recommend_video) {
+            console.log("recommend_video🚁🚁🚁");
+          }
+          // 点击相应卡片
+          recommend_video.click();
+          return;
+        } catch (error) {
+          console.log(
+            "%cerror%c:",
+            "background:#ff0000;padding:3px;border-radius:3px",
+            "",
+            error
+          );
+        }
+        // 获取搜索页面的视频卡片
+        try {
+          let search_video = document.querySelectorAll(
+            ".bili-video-card__wrap"
+          )[num].children[0];
+          console.log("recommend_video🚁🚁🚁", search_video);
+          // 点击相应卡片
+          search_video.click();
+        } catch (error) {
+          console.log(
+            "%cerror%c:",
+            "background:#ff0000;padding:3px;border-radius:3px",
+            "",
+            error
+          );
+        }
+      }
+    });
+  }
+  function searchPageSelectCard() {
+    document.addEventListener("keydown", function (event) {
+      // 检测到光标聚焦于输入框时，则不触发快捷键事件。
+      if (
+        ["input", "textarea"].indexOf(event.target.tagName.toLowerCase()) > -1
+      ) {
+        return false;
+      }
+      //   监听数字键盘
+      if (event.keyCode >= 48 && event.keyCode <= 57 && !event.ctrlKey) {
+        event.preventDefault();
+        let num = event.keyCode - 48 > 0 ? event.keyCode - 48 : 10;
+        // 获取搜索页面的视频卡片
+        try {
+          let search_video = document.querySelectorAll(
+            ".bili-video-card__wrap"
+          )[num].children[0];
+          console.log("recommend_video🚁🚁🚁", search_video);
+          // 点击相应卡片
+          search_video.click();
+        } catch (error) {
+          console.log(
+            "%cerror%c:",
+            "background:#ff0000;padding:3px;border-radius:3px",
+            "",
+            error
+          );
+        }
       }
     });
   }
